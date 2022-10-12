8f7a6f3a7bcc17c0f22f6aecf96240e7480c1937
jiangby <jiangby@rp-pet.cn>
2022-10-12 15:14:35 +0800

    [bug] start service will kill gunicorn service

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,15 @@
 #! /usr/bin/bash
-already_run=$(lsof -t -i:6400)
+already_run=($(lsof -t -i:6400))
 if [ ! -z "$already_run" ]; then
     echo $already_run
-    killall -9 "$already_run"
+    for proc_id in ${already_run[@]}
+    do
+        echo $proc_id
+        kill -9 $proc_id
+    done
 fi
-pkill gunicorn 
+# pkill gunicorn 
 
-sleep 5 &
+sleep 3 &
 wait $!
 gunicorn -c conf.py app:app
